d28516fbd809a86ef2497481d3cbb1e59174831a
freestrokes <freestrokes.hk@gmail.com>
2023-07-22 18:05:13 +0900

    [main] Softeer Virus 문제 풀이 수정

--- java-project/src/com/freestrokes/practice/softeer/Virus.java
+++ java-project/src/com/freestrokes/practice/softeer/Virus.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.io.*;
 
 /**
- * Virus
+ * 바이러스 (Virus)
  *
  * 문제:
  * 바이러스가 숙주의 몸속에서 1초당 P배씩 증가한다.
@@ -39,23 +39,25 @@ public class Virus {
 
     public static void main(String[] args) throws IOException {
 
-        // Scanner와의 입력 속도를 비교해보면 BufferedReader가 더 빠르게 동작한다.
+        // BufferedReaderd와 Scanner의 입력 속도를 비교해보면 BufferedReader가 더 빠르게 동작한다.
         // 적은 양의 데이터인 경우엔 문제가 없겠지만 데이터 양이 많아질수록 성능 차이는 크게 나타난다.
         // BufferedReader가 버퍼에 입력값을 버퍼에 저장했다가 전송하기 때문에 더 빠르다.
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
 
-        int k = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken()); // 바이러스의 수
+        int p = Integer.parseInt(st.nextToken()); // 증가율
+        int n = Integer.parseInt(st.nextToken()); // 시간 (초)
         long result = k;
 
         // TODO: CASE1)
+        // for문을 사용한 반복.
 //        for(int i = 1; i <= n; i++) {
 //            result = (result * p) % 1000000007;
 //        }
 
         // TODO: CASE2)
+        // 재귀함수를 이용한 분할정복 사용.
         result = virusCalc(k, p, n);
         System.out.println(result);
 
@@ -65,6 +67,11 @@ public class Virus {
         if (n == 0) {
             return k;
         } else {
+            // 모듈러(Modular) 연산을 사용.
+            // 연산 결과를 계속 1000000007로 나누어 줌.
+            // 최종적으로 출력할 값은 연산 결과를 1000000007롤 나눈 나머지 값이기 때문에
+            // 계속 1000000007로 나누어서 나머지 값을 반환해줌.
+            // 모듈러 연산 예시 -> (a * b) % M = ( (a % M) * (b % M) ) % M
             k = (k * p) % 1000000007;
             n--;
             return virusCalc(k, p, n);
